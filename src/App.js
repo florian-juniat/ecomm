417397bb91abe3cc8960047d40af8468934c4266
florian-juniat <florianjuniat@gmail.com>
2021-03-07 19:53:15 +0000

    push

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+import axios from 'axios';
+
 
 function App() {
+  
+  const [test, setTest] = useState("Nothing")
+
+  const handleClick = () => {
+
+    axios.get('https://back-ecommerce01.herokuapp.com/')
+      .then(res => {
+        console.log(res)
+        setTest(res.data);
+      })
+  }
+
   return (
     <div className="App">
       <header className="App-header">
           <h1 className="title">E-commerce</h1>
       </header>
       <div className="mybody">
-        <button className="ButtonTest">
+        <button className="ButtonTest" onClick={handleClick}>
             Verify
         </button>
       </div>
+      <p className="myP">{test}</p>
     </div>
   );
 }
