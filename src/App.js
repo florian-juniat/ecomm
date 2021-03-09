442ddc5c793cd2187430811ba5735c9cf3f39d3b
florian-juniat <florianjuniat@gmail.com>
2021-03-09 13:02:37 +0000

    push

--- src/App.js
+++ src/App.js
@@ -9,17 +9,72 @@ function App() {
   
   const [test, setTest] = useState("Nothing")
   const [userName, setUserName] = useState("")
+  const [userPass, setUserPass] = useState("")
+  const [token, setToken] = useState("")
+
+  const [userNameRegister, setUserNameRegister] = useState("")
+  const [userPassRegister, setUserPassRegister] = useState("")
 
   const handleClick = () => {
-    axios.get('https://back-ecommerce01.herokuapp.com/')
+    axios.get('https://back-ecommerce01.herokuapp.com/auth/verify', {headers: {
+      'Authorization': token }
+    })
       .then(res => {
         console.log(res)
-        setTest(res.data);
-      })
+        setTest(res.data.name);
+      }).catch(function (error) {
+        if (error.response) {
+          setTest("Not Connected");
+        }
+      });
+  }
+
+
+  const handleClickLogin = () => {
+    axios.post('https://back-ecommerce01.herokuapp.com/auth/login', {
+      name: userName,
+      password: userPass
+    }).then(res => {
+        console.log(res)
+        setToken(res.data)
+        setTest("Succeed Connection");
+    }).catch(function (error) {
+      if (error.response) {
+        setTest("Connection Refused");
+      }
+    });
+  }
+
+
+  const handleClickRegister = () => {
+    axios.post('https://back-ecommerce01.herokuapp.com/auth/register', {
+      name: userNameRegister,
+      password: userPassRegister
+    }).then(res => {
+        console.log(res)
+        setTest("Succeed Registration");
+    }).catch(function (error) {
+      if (error.response) {
+        console.log(error.response)
+        setTest("Registration Refused");
+      }
+    });
   }
 
   const handleWriteUser = (event) => {
     setUserName(event.target.value);
+  }
+
+  const handleWritePass = (event) => {
+    setUserPass(event.target.value);
+  }
+
+  const handleWriteUserRegister = (event) => {
+    setUserNameRegister(event.target.value);
+  }
+
+  const handleWritePassRegister = (event) => {
+    setUserPassRegister(event.target.value);
   }
 
   
@@ -30,21 +85,48 @@ function App() {
         <header className="App-header">
             <h1 className="title">E-commerce</h1>
         </header>
-        <div className="mybody">
-          <div className="containerInput">
-            <h2 className="titleInput">Username</h2>
-            <input className="myInput" type="text" value={userName} onChange={handleWriteUser} />
+
+
+        <div className="handleLine">
+          <div className="mybody">
+            <h2 className="titleSection">Login</h2>
+            <div className="containerInput">
+              <h2 className="titleInput">Username</h2>
+              <input className="myInput" type="text" value={userName} onChange={handleWriteUser} />
+            </div>
+            <div className="containerInput">
+              <h2 className="titleInput">Password</h2>
+              <input className="myInput" type="text" value={userPass} onChange={handleWritePass} />
+            </div>
+            <button className="ButtonTest" onClick={handleClickLogin}>
+                Sign in
+            </button>
+            
           </div>
-          <div className="containerInput">
-            <h2 className="titleInput">Password</h2>
-            <input className="myInput" type="text" value={userName} onChange={handleWriteUser} />
+
+          <div className="mybody">
+            <h2 className="titleSection">Sign Up</h2>
+            <div className="containerInput">
+              <h2 className="titleInput">Username</h2>
+              <input className="myInput" type="text" value={userNameRegister} onChange={handleWriteUserRegister} />
+            </div>
+            <div className="containerInput">
+              <h2 className="titleInput">Password</h2>
+              <input className="myInput" type="text" value={userPassRegister} onChange={handleWritePassRegister} />
+            </div>
+            <button className="ButtonTest" onClick={handleClickRegister}>
+                Sign up
+            </button>
+            
           </div>
-          <button className="ButtonTest" onClick={handleClick}>
-              Connect
-          </button>
-          
         </div>
+
+
+        <button className="ButtonTest" onClick={handleClick}>
+              Verify Connection
+          </button>
         <p className="myP">{test}</p>
+        <p className="myP">{token == "" ? "" : "Connected"}</p>
       </div>
     </div>
     
