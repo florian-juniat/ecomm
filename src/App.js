f2d71614b3f7b9fdf92471c890176e32d1409380
florian-juniat <florianjuniat@gmail.com>
2021-03-19 11:49:32 +0000

    test

--- src/App.js
+++ src/App.js
@@ -1,58 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import { CssBaseline } from '@material-ui/core';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+import { Navbar, Products, Cart, Checkout } from './components';
+import { commerce } from './lib/commerce';
 
-import Test from './pages/Test'
-import Home from './pages/Home'
-import Login from './pages/Login'
-import Contact from './pages/Contact'
-import SignUp from './pages/SignUp'
-import HeaderStart from './utility/HeaderStart'
+const App = () => {
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState({});
+  const [order, setOrder] = useState({});
+  const [errorMessage, setErrorMessage] = useState('');
 
-import HomePage from './pages/HomePage'
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
 
-import Paiement from './pages/Paiement'
+    setProducts(data);
+  };
 
-function App() {
+  const fetchCart = async () => {
+    setCart(await commerce.cart.retrieve());
+  };
 
-  const [page, setPage] = useState("home")
-  const [token, setToken] = useState("")
+  const handleAddToCart = async (productId, quantity) => {
+    const item = await commerce.cart.add(productId, quantity);
 
-  var display = <Home setPage={setPage}/>
+    setCart(item.cart);
+  };
 
-  if (page == "home") {
-    display = <Home setPage={setPage}/>
-  }
-  if (page == "signin") {
-    display = <Login setPage={setPage} setToken={setToken}/>
-  }
-  if (page == "contact") {
-    display = <Contact setPage={setPage}/>
-  }
+  const handleUpdateCartQty = async (lineItemId, quantity) => {
+    const response = await commerce.cart.update(lineItemId, { quantity });
 
-  if (page == "signup") {
-    display = <SignUp setPage={setPage} setToken={setToken}/>
-  }
+    setCart(response.cart);
+  };
 
-  if (page == "testpaiement") {
-    display = <Paiement setPage={setPage} setToken={setToken}/>
-  }
+  const handleRemoveFromCart = async (lineItemId) => {
+    const response = await commerce.cart.remove(lineItemId);
 
-  if (page == "HomePage" && token != "") {
-    display = <HomePage setPage={setPage} setToken={setToken} token={token}/>
-  }
-  
+    setCart(response.cart);
+  };
 
+  const handleEmptyCart = async () => {
+    const response = await commerce.cart.empty();
 
+    setCart(response.cart);
+  };
+
+  const refreshCart = async () => {
+    const newCart = await commerce.cart.refresh();
+
+    setCart(newCart);
+  };
+
+  const handleCaptureCheckout = async (checkoutTokenId, newOrder) => {
+    try {
+      const incomingOrder = await commerce.checkout.capture(checkoutTokenId, newOrder);
+
+      setOrder(incomingOrder);
+
+      refreshCart();
+    } catch (error) {
+      setErrorMessage(error.data.error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts();
+    fetchCart();
+  }, []);
+
+  const handleDrawerToggle = () => setMobileOpen(!mobileOpen);
 
   return (
-    <div className="App">
-      {display}
-    </div>
-    
+    <Router>
+      <div style={{ display: 'flex' }}>
+        <CssBaseline />
+        <Navbar totalItems={cart.total_items} handleDrawerToggle={handleDrawerToggle} />
+        <Switch>
+          <Route exact path="/">
+            <Products products={products} onAddToCart={handleAddToCart} handleUpdateCartQty />
+          </Route>
+          <Route exact path="/cart">
+            <Cart cart={cart} onUpdateCartQty={handleUpdateCartQty} onRemoveFromCart={handleRemoveFromCart} onEmptyCart={handleEmptyCart} />
+          </Route>
+          <Route path="/checkout" exact>
+            <Checkout cart={cart} order={order} onCaptureCheckout={handleCaptureCheckout} error={errorMessage} />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
