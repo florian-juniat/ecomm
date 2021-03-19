3ffa3769f50252a03ecd27d54444fb3fcc9b35f2
florian-juniat <florianjuniat@gmail.com>
2021-03-19 11:50:11 +0000

    Updates

---
+++ precache-manifest.e6dc1ec02115dfa1aab3f890f5249ba6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ff20d8faeaaaca90e7378353f86f977",
+    "url": "/ecomm/index.html"
+  },
+  {
+    "revision": "95853e7c506986684440",
+    "url": "/ecomm/static/js/2.b7c953be.chunk.js"
+  },
+  {
+    "revision": "70af1bffd51a7281193fa8f9b7319597",
+    "url": "/ecomm/static/js/2.b7c953be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18790e9845067f157a27",
+    "url": "/ecomm/static/js/main.b92ee54b.chunk.js"
+  },
+  {
+    "revision": "c90edb3b80fb23f8a1df",
+    "url": "/ecomm/static/js/runtime-main.a361016d.js"
+  },
+  {
+    "revision": "422fec1079cce450353e924288f8313b",
+    "url": "/ecomm/static/media/commerce.422fec10.png"
+  }
+]);
