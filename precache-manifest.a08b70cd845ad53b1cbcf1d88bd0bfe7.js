d5d56260f725c60c87fed3ca58c9358c302a40b6
florian-juniat <florianjuniat@gmail.com>
2021-03-25 12:44:51 +0000

    Updates

--- precache-manifest.a08b70cd845ad53b1cbcf1d88bd0bfe7.js
+++ precache-manifest.a08b70cd845ad53b1cbcf1d88bd0bfe7.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "dcdb8a174eb79ab3aaf7f10455458307",
+    "revision": "da22e7ab422253cbc1be20b1609752d5",
     "url": "/ecomm/index.html"
   },
   {
-    "revision": "e2616252597463a2c9ec",
-    "url": "/ecomm/static/js/2.374ad2ad.chunk.js"
+    "revision": "d2b5b27e3b29ac6ad8ca",
+    "url": "/ecomm/static/js/2.57cbb235.chunk.js"
   },
   {
     "revision": "70af1bffd51a7281193fa8f9b7319597",
-    "url": "/ecomm/static/js/2.374ad2ad.chunk.js.LICENSE.txt"
+    "url": "/ecomm/static/js/2.57cbb235.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8545b7533dc2f6b73043",
-    "url": "/ecomm/static/js/main.1988aa31.chunk.js"
+    "revision": "ed91d0fd65bc36e36781",
+    "url": "/ecomm/static/js/main.ca9443d6.chunk.js"
   },
   {
     "revision": "c90edb3b80fb23f8a1df",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ecomm/precache-manifest.828d7598b1f59d98756f13006af67f27.js"
+  "/ecomm/precache-manifest.a08b70cd845ad53b1cbcf1d88bd0bfe7.js"
 );
 
 self.addEventListener('message', (event) => {
